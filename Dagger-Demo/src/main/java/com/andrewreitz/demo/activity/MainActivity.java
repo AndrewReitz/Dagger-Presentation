238839e04cba56a394a982de432e2b54cc0f8357
Andrew Reitz <areitz@nerdery.com>
2014-01-29 12:52:55 -0600

    cleaning up code

--- Dagger-Demo/src/main/java/com/andrewreitz/demo/activity/MainActivity.java
+++ Dagger-Demo/src/main/java/com/andrewreitz/demo/activity/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.inject(this);
 
         mNinja.attack("pirate");
     }
